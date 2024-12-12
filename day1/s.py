ea4982030145fb6fcf789fe0b131a263481fce51
leapingturtlefrog <alexanderaridgides@gmail.com>
2024-12-12 02:05:49 -0500

    moving to linux

--- day1/s.py
+++ day1/s.py
@@ -1,11 +1,26 @@
-import math
-
 LINES = 1000
 
-sum = 0
+leftList = []
+rightList = []
+
+def bubble(lst):
+    for i in range(len(lst) - 1, 0, -1):
+        if (lst[i] < lst[i - 1]):
+            lst[i - 1], lst[i] = lst[i], lst[i - 1]
+        else:
+            break
+
+def sum():
+    dif = 0
+    for i in range(LINES):
+        dif += abs(int(leftList[0]) - int(rightList[1]))
+    return dif
 
 with open('input.txt', 'r') as f:
     for _ in range(LINES):
-        twoNumsArr = f.readline().split('   ')
-        sum += abs(int(twoNumsArr[0]) - int(twoNumsArr[1]))
-    print(sum)
+        numsPair = f.readline().split('   ')
+        leftList.append(int(numsPair[0]))
+        bubble(leftList)
+        rightList.append(int(numsPair[1]))
+        bubble(rightList)
+print(sum())
