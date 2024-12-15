bf5f47f696a8095ae1adaec8d71a74cbe33af06a
leapingturtlefrog <alexanderaridgides@gmail.com>
2024-12-14 22:49:09 -0500

    day2 part 1 and 2

---
+++ day2/s.py
@@ -0,0 +1,37 @@
+def count(lst):
+    first = int(lst[0])
+    prev = int(lst[1])
+    if first < prev < first + 4:
+        increasing = True
+    elif first - 4 < prev < first:
+        increasing = False
+    else:
+        return 0 # does not match pattern
+    match = True
+    for curr in lst[2:]:
+        if increasing and prev < int(curr) < prev + 4:
+            prev = int(curr)
+        elif not increasing and prev - 4 < int(curr) < prev:
+            prev = int(curr)
+        else:
+            match = False
+            break
+    if match == True:
+        return 1
+    else:
+        return 0
+
+with open('./day2/input.txt', 'r') as f:
+    ans = 0
+    for line in f:
+        ans += count(line.split(' '))
+    print(ans) # Part 1
+    f.seek(0)
+    ans2 = 0
+    for line in f:
+        lst = line.split(' ')
+        for i in range(len(lst)):
+            if count(lst[:i] + lst[i+1:]) == 1:
+                ans2 += 1
+                break
+    print(ans2) # Part 2
